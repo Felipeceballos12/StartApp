03412a6281a090a3741467754c8b109ec09a7dd0
Juan <j.contrerasa94@gmail.com>
2021-09-27 14:06:03 -0500

    Crear arquitectura de app

---
+++ VetHouse.App.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetHouse.App.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ VetHouse.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetHouse.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
