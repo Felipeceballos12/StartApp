0f7a31b370922b12c36836b15806c846943c6ec6
Juan <j.contrerasa94@gmail.com>
2021-09-28 09:54:32 -0500

    Agregar entidad CareSuggestions

---
+++ VetHouse.App.Dominio/Entidades/CareSuggestions.cs
@@ -0,0 +1,9 @@
+namespace VetHouse.App.Dominio
+{
+    public class CareSuggestions
+    {
+        public int Id {get;set;}
+        public string Description {get;set;}
+        public DateTime CreatedAt {get;set;}
+    }
+}
