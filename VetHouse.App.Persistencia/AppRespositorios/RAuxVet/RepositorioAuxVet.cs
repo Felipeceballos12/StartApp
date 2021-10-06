691052edc1f7d7852eb75b56aa20a0851cb817bc
Rudarkia <rudarkia@gmail.com>
2021-10-06 10:08:33 -0500

    Se completa código de los repositorios y la interface de Vet y AuxVet

---
+++ VetHouse.App.Persistencia/AppRespositorios/RAuxVet/RepositorioAuxVet.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using VetHouse.App.Dominio;
+
+namespace VetHouse.App.Persistencia
+{
+    public class RepositorioAuxVet : IRepositorioAuxVet
+    {
+        /// <summary>
+        /// Referencia al contexto del AuxVet
+        /// </summary>
+        private readonly AppContext _appContext;
+        /// <summary>
+        /// Metodo constructor utiliza
+        /// Inyeccion de dependencia para indicar el contexto a utilizar 
+        /// </summary>
+        ///<param name= "appContext"></param>//
+        public RepositorioAuxVet (AppContext appContext)
+        {
+            _appContext=appContext;
+        }
+        AuxVet IRepositorioAuxVet.AddAuxVet(AuxVet auxVet)
+        {
+            var auxVetAdicionado = _appContext.AuxVet.Add(auxVet);
+            _appContext.SaveChanges();
+            return auxVetAdicionado.Entity;
+        }
+        void IRepositorioAuxVet.DeleteAuxVet(int idAuxVet)
+        {
+            var auxVetEncontrado = _appContext.AuxVet.FirstOrDefault(av => av.Id == idAuxVet);
+            if (auxVetEncontrado == null)
+                return;
+            _appContext.AuxVet.Remove(auxVetEncontrado);
+            _appContext.SaveChanges();
+        }
+        IEnumerable<AuxVet> IRepositorioAuxVet.GetAllAuxVet()
+        {
+            return _appContext.AuxVet;
+        }
+        AuxVet IRepositorioAuxVet.GetAuxVet(int idAuxVet)
+        {
+            return _appContext.AuxVet.FirstOrDefault(p => p.Id == idAuxVet);
+        }
+        AuxVet IRepositorioAuxVet.UpdateAuxVet(AuxVet auxVet)
+        {
+            var auxVetEncontrado = _appContext.AuxVet.FirstOrDefault(p => p.Id == auxVet.Id);
+            if (auxVetEncontrado!=null)
+            {
+                auxVetEncontrado.Name=auxVet.Name;
+                auxVetEncontrado.Surname=auxVet.Surname;
+                auxVetEncontrado.PhoneNumber=auxVet.PhoneNumber;
+                auxVetEncontrado.Gender=auxVet.Gender;
+                auxVetEncontrado.Email=auxVet.Email;               
+                auxVetEncontrado.ProfessionalCard=auxVet.ProfessionalCard;
+                auxVetEncontrado.LaborHours=auxVet.LaborHours;
+            }
+            return auxVetEncontrado;
+        }
+    }
+}
---
+++ VetHouse.App.Persistencia/AppRespositorios/RVet/RepositorioVet.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using VetHouse.App.Dominio;
+
+namespace VetHouse.App.Persistencia
+{
+    public class RepositorioVet : IRepositorioVet
+    {
+        /// <summary>
+        /// Referencia al contexto del Vet
+        /// </summary>
+        private readonly AppContext _appContext;
+        /// <summary>
+        /// Metodo constructor utiliza
+        /// Inyeccion de dependencia para indicar el contexto a utilizar 
+        /// </summary>
+        ///<param name= "appContext"></param>//
+        public RepositorioVet (AppContext appContext)
+        {
+            _appContext=appContext;
+        }
+        Vet IRepositorioVet.AddVet(Vet vet)
+        {
+            var vetAdicionado = _appContext.Vet.Add(vet);
+            _appContext.SaveChanges();
+            return vetAdicionado.Entity;
+        }
+        void IRepositorioVet.DeleteVet(int idVet)
+        {
+            var vetEncontrado = _appContext.Vet.FirstOrDefault(av => av.Id == idVet);
+            if (vetEncontrado == null)
+                return;
+            _appContext.Vet.Remove(vetEncontrado);
+            _appContext.SaveChanges();
+        }
+        IEnumerable<Vet> IRepositorioVet.GetAllVet()
+        {
+            return _appContext.Vet;
+        }
+        Vet IRepositorioVet.GetVet(int idVet)
+        {
+            return _appContext.Vet.FirstOrDefault(av => av.Id == idVet);
+        }
+        Vet IRepositorioVet.UpdateVet(Vet vet)
+        {
+            var vetEncontrado = _appContext.Vet.FirstOrDefault(av => av.Id == vet.Id);
+            if (vetEncontrado!=null)
+            {
+                vetEncontrado.Name=vet.Name;
+                vetEncontrado.Surname=vet.Surname;
+                vetEncontrado.PhoneNumber=vet.PhoneNumber;
+                vetEncontrado.Gender=vet.Gender;
+                vetEncontrado.Email=vet.Email;    
+                vetEncontrado.RegisterRethus = vet.RegisterRethus;
+                vetEncontrado.Specialty=vet.Specialty;
+            }
+            return vetEncontrado;
+        }
+    }
+}
