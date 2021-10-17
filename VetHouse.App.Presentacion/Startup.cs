2e467f90d7ae74aad4d424ba84e1e4e11ef728b1
Luis Felipe Ceballos Caicedo <ceballos-65@hotmail.com>
2021-10-17 10:27:50 -0500

    Agregue el AddSigleton  y tambien agregue bootstrap a nuestro proyecto para trabajar de una mejor manera

--- VetHouse.App.Presentacion/Startup.cs
+++ VetHouse.App.Presentacion/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VetHouse.App.Persistencia;
 
 namespace VetHouse.App.Presentacion
 {
@@ -24,6 +25,12 @@ namespace VetHouse.App.Presentacion
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRepositorioOwner, RepositorioOwner>();
+            services.AddSingleton<IRepositorioVet, RepositorioVet>();
+            services.AddSingleton<IRepositorioPet, RepositorioPet>();
+            services.AddSingleton<IRepositorioVitalSign, RepositorioVitalSign>();
+            services.AddSingleton<IRepositorioHistory, RepositorioHistory>();
+            services.AddSingleton<IRepositorioCareSuggestion, RepositorioCareSuggestion>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
